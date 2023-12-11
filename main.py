ec55816219faa750091621b0407e5f30f9e1f0df
jackoubridge <jaoubridge@gmail.com>
2023-12-11 06:08:16 +0000

    don't think any more optimisations are possible. goodnight

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ def extract_costs(xml_file):
 
     return np.array(distances), cities
 
-
 def generate_path(num_cities, start, pheromones, distances):
     """
     Description: Calculates the path of an ant.
@@ -40,17 +39,14 @@ def generate_path(num_cities, start, pheromones, distances):
     path = np.array([start])  # Set the path equal to only the start node
     all_cities = np.arange(0, num_cities)  # Create an ordered list of all nodes
 
-    for city in range(1, num_cities):
+    for _ in range(1, num_cities):
         unvisited = np.delete(all_cities, path)  # Remove visited nodes
-        if len(unvisited) == 1:
-            choice = unvisited[0]
-        else:
-            next_pheromones = np.delete(pheromones[path[-1]], path)  # Get pheromones for all potential next nodes
-            next_edges = np.delete(distances[path[-1]], path)
-            heuristic = np.divide(np.divide(next_pheromones, next_edges), np.divide(np.sum(next_pheromones), np.sum(next_edges)))
-            
-            choice = random.choices(unvisited, weights=heuristic, cum_weights=None,k=1)  # Picks a next city at random, but weighted
-        path = np.append(path, choice)  # Add city to path
+        next_pheromones = np.delete(pheromones[path[-1]], path)  # Get pheromones for all potential next nodes
+        
+        next_edges = np.delete(distances[path[-1]], path) # Get distances to all potential next nodes
+        heuristic = np.divide(np.divide(next_pheromones, next_edges), np.divide(np.sum(next_pheromones), np.sum(next_edges))) # Calculate heuristic value
+       
+        path = np.append(path, random.choices(unvisited, weights=heuristic, cum_weights=None,k=1))  # Picks a next city at random, but weighted
     return path
 
 def update_pheromones(reward, path, n):
@@ -61,57 +57,57 @@ def update_pheromones(reward, path, n):
                 An integer (n), that denotes the number of nodes in the graph
     Returns: A numpy array (p), in the form of a matrix containing the evaporation rate of the pheromones
     """
-    probability = np.zeros((len(path), len(path)), dtype='longdouble')
+    pheromones_update = np.zeros((len(path), len(path)), dtype='longdouble')
     x_indices = path[:-1]
     y_indices = path[1:]
-    probability[x_indices, y_indices] += np.longdouble(reward)
-    return probability
+    pheromones_update[x_indices, y_indices] += np.longdouble(reward)
+    return pheromones_update
 
 def tests():
+    
+    distances, num_cities = extract_costs('burma.xml')  # load the cost matrix and the amount of cities
 
-    test_dic = {'1': [50, 0.5, 0.5], '2': [50, 1.0, 1.0], '3': [50, 1.5, 1.5],
-                '4': [100, 0.5, 0.5], '5': [100, 1.0, 1.0], '6': [100, 1.5, 1.5],
-                '7': [150, 0.5, 0.5], '8': [150, 1.0, 1.0], '9': [150, 1.5, 1.5]}      
+    test_dic = {'1': [10, 0.25, 0.25], '2': [10, 0.5, 0.5], '3': [10, 1.5, 1.5],
+                '4': [50, 0.25, 0.25], '5': [50, 0.5, 0.5], '6': [50, 1.5, 1.5],
+                '7': [100, 0.25, 0.25], '8': [100, 0.5, 0.5], '9': [100, 1.5, 1.5]}      
     
     fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(14, 7), gridspec_kw={'hspace': 0.6, 'wspace': 0.4})
-    fig.tight_layout()
     
     for i, ax in enumerate(fig.axes):
+        fitness_list = []
         row = test_dic[str(i + 1)]
         ax.set_xlabel('Iterations')
         ax.set_ylabel('Fitness')
-        fitness_list = main(row[0], row[1], row[2])
-        ax.set_title(label="M=%s, E=%s, Q=%s | Best=%s" % (row[0], row[1], row[2], int(min(fitness_list))))
-        ax.plot(np.arange(len(fitness_list)), fitness_list)
+        for j in tqdm(range(10)):
+            fitness_list.append(main(distances, num_cities, row[0], row[1], row[2]))
+        averages = [sum(x) / len(x) for x in zip(*fitness_list)]
+        ax.set_title(label="M=%s, E=%s, Q=%s | Best=%s" % (row[0], row[1], row[2], int(min(averages))))
+        ax.plot(np.arange(len(averages)), averages)
 
     plt.show()
-                
 
-def main(m, e, Q):
-    distances, num_cities = extract_costs('burma.xml')  # load the cost matrix and the amount of cities
+def main(distances, num_cities, M, E, Q):
     pheromones = np.random.rand(num_cities, num_cities)  # Initialise random pheromones
     np.fill_diagonal(pheromones, 0)
-    iterations = int(10000 / m)  # Easier to keep track of iterations, given by the number of evaluations divided by the number of ants
+    iterations = int(10000 / M)  # Easier to keep track of iterations, given by the number of evaluations divided by the number of ants
 
     start_node = np.random.randint(num_cities)  # Randomly pick a start node
     fitness_list = []  # List that will store the average fitness per iteration
 
-    for x in tqdm(range(iterations)):
+    for _ in (range(iterations)):
         total_fitness = 0
         pheromone_update = np.zeros((num_cities, num_cities), dtype='longdouble')  # no pheromone drops as no ants have moved
 
-        for ant in range(m):
-            path = generate_path(num_cities, start_node, pheromones, distances)  # Generate the path
+        for _ in range(M):
+            path = generate_path(num_cities, start_node, pheromones, distances)
             fitness = np.sum(distances[path[:-1], path[1:]])
-
             total_fitness += fitness
             reward = np.longdouble(Q) / np.longdouble(fitness)
             pheromone_update = np.add(pheromone_update, update_pheromones(reward, path, num_cities))
 
-        pheromones = np.add(pheromones, pheromone_update)
-        pheromones *= e
+        pheromones = np.add(pheromones, pheromone_update) * E
 
-        fitness_list.append(total_fitness / m)
+        fitness_list.append(total_fitness / M)
 
     return fitness_list
 
